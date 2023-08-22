4dde33493bc198806379b6cd2a7bac69da3791e8
AIVAF <metrix.add@yandex.ru>
2023-08-22 19:48:12 +0100

    Adjusted tests.py and request.py filed as per the recommendation, thank you for your feedback!

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 CREATE_ORDER_PATH = '/api/v1/orders'
 GET_ORDER_PATH = '/api/v1/orders/track'
-URL_SERVICE = 'https://0c0c2392-3b78-44cb-869b-a706c6ddde4f.serverhub.praktikum-services.ru'
+URL_SERVICE = 'https://3ccb1c31-b4a3-4a46-a12b-d3ce1daed2f1.serverhub.praktikum-services.ru'
--- tests.py
+++ tests.py
@@ -3,6 +3,28 @@ import request
 
 # Анна Иванченко, 7-й  поток — Финальный проект. Инженер по тестированию плюс
 
-def test_status_code():
-    assert request.response.status_code == 200
+# Функция проверки успешного создания заказа
+def positive_assert_new_order(body):
+    response = request.post_new_order(body)
+    
+    assert response.status_code == 201
+
+# Проверяем успешность создания заказа с телом из файла data
+def test_success_response():
+    positive_assert_new_order(data.order_body)
+
+# Функция проверки кода ответа
+def positive_assert_track_number(number):
+    response_order = request.get_order_tracknumber(number)
+
+    assert response_order.status_code == 200
+
+# Вытаскиваем трек-номер при создании заказа и проверяем код ответа
+def test_success_response_number():
+    response_order = request.post_new_order(data.order_body)
+    track_number = response_order.json()["track"]
+    positive_assert_track_number(track_number)
+
+
+
 
